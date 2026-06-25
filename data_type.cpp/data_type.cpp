99615add96ca23c45c86bb163e310bb3d9755b94
AryanHQ <yesveer.singh979803@gmail.com>
2026-06-26 00:33:10 +0530

    data types

---
+++ data_type.cpp/data_type.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+
+    int Tea_leaves = 50;
+    float Water_temperature = 85.5;
+    double Price_of_tea = 299.99;
+    char Tea_grade = 'A';
+    bool Is_tea_ready = false;
+
+    cout << Is_tea_ready << endl;
+    cout << Tea_grade << endl;
+    cout << Price_of_tea << endl;
+    cout << Water_temperature << endl;
+    cout << Tea_leaves << endl;
+
+    return 0;
+}
