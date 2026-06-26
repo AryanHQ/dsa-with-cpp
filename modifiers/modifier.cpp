9c88804ac1d752352e5c72a063b19311f7315950
AryanHQ <aryancodebase@gmail.com>
2026-06-27 00:17:33 +0530

    Modifiers

---
+++ modifiers/modifier.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+
+    int Small_tea_pack = 100; // This can print -100 if we don't want to print -100 we use unsigned
+    // unsigned Small_tea_pack = -100;
+
+    long Large_tea_storage = 100000000; // For more large number we use long long
+
+    short Tea_sample = 9;
+
+    cout << Small_tea_pack << endl;
+    cout << Large_tea_storage << endl;
+
+    return 0;
+}
