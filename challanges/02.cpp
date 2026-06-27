5c6508c425d703c4d3bc624977cbd2726ec26b2c
AryanHQ <aryancodebase@gmail.com>
2026-06-28 00:51:44 +0530

    Challenges

---
+++ challanges/02.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+using std::cout;
+
+using std ::endl;
+
+int main()
+{
+    cout << "Hello jii" << endl;
+
+    // std :: cout << "Hello jii" << std::endl;  "This is also correct"
+
+    return 0;
+}
---
+++ challanges/03.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+ using namespace std;
+
+ int main(){
+
+    int score;
+    score = 110;
+
+    // const int uid = 827858; we can't chane the variabl here because we use const
+    const int uid = 34324333335;
+    
+    
+    
+    return 0;
+    
+    
+}
+//  cout << "Wellcome to chai with aryan" <<endl ;
+//  cout << "Wellcome to chai with aman" <<endl ;
+//  cout << "Wellcome to chai with amit" <<endl ;
