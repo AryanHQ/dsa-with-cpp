47347691074c16c7b52776d8cc6d813d9d6afc0a
AryanHQ <aryancodebase@gmail.com>
2026-06-29 00:55:51 +0530

    problem 05

---
+++ challanges/05.cpp
@@ -0,0 +1,24 @@
+// Challenge 2: Assignment Operators
+// Create a program where the user inputs the number of tea bags they have. If the number is less than 20, give them 10 extra bags using the += assignment operator. Display the updated total.
+
+#include <iostream>
+
+using namespace std;
+
+int main() {
+
+    int Tea_bags;
+
+    cout << "How many tea bags you have: ";
+    cin >> Tea_bags;
+
+    if (Tea_bags < 20)
+    {
+        Tea_bags+=10;
+
+        cout << "Here is your extra 10 bags: " << Tea_bags << endl;
+    }else{
+        cout << "You have " << Tea_bags << " tea bags" << endl;
+    }    
+    return 0;
+}
