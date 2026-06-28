f6f7810fd1b4ccd0142d6f789449d3e7b5295879
AryanHQ <aryancodebase@gmail.com>
2026-06-29 00:18:03 +0530

    strings

---
+++ strings/string.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main() {
+
+    string Fav_tea = "Green tea \n";
+    string Discription = "Known as \"healthy\" tea \n";
+
+    cout << Fav_tea << Discription << endl;
+
+    // cout << Discription;
+
+
+
+    return 0;
+}
