8b8e411aac34319eea2ca614f1c8948d0a1490d4
AryanHQ <aryancodex.py@gmail.com>
2026-07-09 03:11:42 +0530

    arrays

---
+++ Array/array.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    int chai_served[7] = {67,23,54,84,34,65,12};
+
+    cout << "Chai cups served on day 1: " << chai_served[0] << endl;
+    cout << "Chai cups served on day 2: " << chai_served[1] << endl;
+    cout << "Chai cups served on day 3: " << chai_served[2] << endl;
+    cout << "Chai cups served on day 4: " << chai_served[3] << endl;
+    cout << "Chai cups served on day 5: " << chai_served[4] << endl;
+    cout << "Chai cups served on day 6: " << chai_served[5] << endl;
+    cout << "Chai cups served on day 7: " << chai_served[6] << endl;
+     return 0;
+}
