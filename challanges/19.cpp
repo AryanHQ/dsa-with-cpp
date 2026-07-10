b49c296652f7ce8670a0fdcfaa20624ee5289a25
AryanHQ <aryancodex.py@gmail.com>
2026-07-10 17:49:36 +0530

    Q19

---
+++ challanges/19.cpp
@@ -0,0 +1,18 @@
+// Challenge 3: For Loop
+// Create a program that prints the first 10 multiples of 2, representing the total tea bags required for making batches of tea.
+
+#include <iostream>
+
+using namespace std;
+
+int main() {
+
+    int i;
+    for (i = 1; i <= 10; i++)
+    {
+        cout << 2 << "*" << i << " = " << 2*i << endl;
+        
+    }
+    
+    return 0;
+}
