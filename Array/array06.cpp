106138e2d334c9a006503accb3fd299663b6778c
AryanHQ <aryancodex.py@gmail.com>
2026-07-24 04:27:44 +0530

    06

---
+++ Array/array06.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    int Laptop_price[4]{45000, 62000, 78000, 55000};
+    cout << "Laptop 1 price: " << "₹"<<Laptop_price[0] << endl;
+    cout << "Laptop 2 price: " << "₹"<<Laptop_price[1] << endl;
+    cout << "Laptop 3 price: " << "₹"<<Laptop_price[2] << endl;
+    cout << "Laptop 4 price: " << "₹"<<Laptop_price[3] << endl;
+
+    cout << "Expensive laptop price: " << "₹"<<Laptop_price[2] << endl;
+
+   
+
+
+
+
+
+    return 0;
+}
