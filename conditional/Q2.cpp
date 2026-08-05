34b670ab6038a06d5f2239619203ac1dea562e48
AryanHQ <aryancodex.py@gmail.com>
2026-08-05 23:13:34 +0530

    If-else problem2

---
+++ conditional/Q2.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int number;
+    cout << "Enter the number : ";
+    cin >> number;
+
+    if (number > 0)
+    {
+        cout << "Positive" << endl;
+    }
+    else if (number < 0)
+    {
+        cout << "Negative" << endl;
+    }
+    else
+    {
+        cout << "Zero" << endl;
+    }
+    return 0;
+}
