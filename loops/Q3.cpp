b240e3bbe7f0de42b7168c5d0801420ac6100cb5
AryanHQ <aryancodex.py@gmail.com>
2026-08-13 21:36:54 +0530

    Q3

---
+++ loops/Q3.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    int i = 1;
+    int j;
+    cout << "j = ";
+    cin >> j;
+    while (i <= j)
+    {
+       cout << i << endl;
+       i++;
+    }
+    
+}
