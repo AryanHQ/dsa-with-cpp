2269055af1efd6afa94c3b6c5e9da73d1c010e88
AryanHQ <aryancodex.py@gmail.com>
2026-09-18 07:50:15 +0530

    Q13

--- loops/Q4.cpp
+++ loops/Q4.cpp
@@ -1,20 +1,21 @@
 #include <iostream>
 using namespace std;
 
-int main() {
- int n;
- cin >> n;
- int lastdigit , sum = 0;
- while (n>0)
- {
-   lastdigit = n%10;
-   sum = sum + lastdigit;
-   n = n/10;
-   
-}
-    cout << sum << "\n";
-  
+int main()
+{
+    int n;
+    cin >> n;
+    int copy = n;
+    int lastdigit, reve = 0;
+    while (n > 0)
+    {
 
+        lastdigit = n % 10;
+        reve = reve * 10 + lastdigit;
+        n = n / 10;
+        
+    }
+    cout << ((reve == copy) ? "Palindrome\n" : "Not Palindrome\n");
 
     return 0;
 }
